f0418ec4f16dfdcc2297e502a00037310e42bfdc
Eva Hernandez <Eva@Dylans-MacBook-Pro.local>
2018-03-20 14:50:42 -0700

    javascript should be working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,16 +12,61 @@
 
   var database = firebase.database();
 
+  var trainNum = 0;
 
-
-  $("#submit").on("click", function() {
+  $("#submit").on("click", function(event) {
 
   	event.preventDefault();
 
   	var name = $("#name").val().trim();
-  	var destination = $("#destination").val().trim();
+  	var destination = $("#desination").val().trim();
   	var time = $("#time").val().trim();
-  	var frequency = parseInt($("#frequency").val().trim());
+  	var frequency = ($("#frequency").val().trim());
 
 
+  	var newTrain = {
+      name: name,
+      destination: destination,
+      frequency: frequency,
+      startingTrain: time,
+    };
+
+    database.ref().push(newTrain);
+
+  	alert("Train information succesfully added");
+
+  	$("#name").val("");
+  	$("#destination").val("");
+  	$("#time").val("")
+  	$("#frequency").val("");
+
+  });
+
+  database.ref().on("child_added", function(snapshot) {
+   
+    var newName = snapshot.val().name;
+    var newDestination = snapshot.val().destination;
+    var newFrequency = snapshot.val().frequency;
+    var firstTime = snapshot.val().startingTrain;
+   
+
+    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+
+    var remainingTime = diffTime % newFrequency;
+    console.log("time %: " +remainingTime);
+
+    var minutesAway = newFrequency - remainingTime;
+
+    var nextTrain = moment().add(minutesAway, "minutes");
+
+    var nextTrainTime = moment(nextTrain).format("HH:mm")
+
+
+    $("#trainInfo").append("<tr><td>" + newName + "</td><td>" + newDestination + "</td><td>" + newFrequency + "</td><td>" + nextTrainTime + "</td><td>" + minutesAway + "</td></tr>");
+
+
+    trainNum++
+    
   });
